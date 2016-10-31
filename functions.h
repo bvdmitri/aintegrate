b219f2b887d040c86ca6a4af2c7f0dbed49955b0
Bagaev Dmitri <bvdmitri@gmail.com>
2016-10-31 15:13:19 +0300

    Fix for icc compiler

--- functions.h
+++ functions.h
@@ -12,11 +12,11 @@ const std::function<double(double)> &f2 = [](double x) {
 	return (sin(sin(sin(sin(sin(sin(x + sqrt(x) + sqrt(sqrt(x)))))))) * cos(cos(cos(cos(cos(log(x + 1)))))));
 };
 
-const std::function<double(double)> &f3 = [](double x) {
+const std::function<double(double)> &f3 = [](double x) -> double {
 	if (x <= 10000) {
 		return (sin(sin(sin(sin(sin(sin(x + sqrt(x) + sqrt(sqrt(x)))))))) * cos(cos(cos(cos(cos(log(x + 1)))))));	
 	} else {
-		return 0.249609;
+		return (double) 0.249609;
 	}
 };
 
